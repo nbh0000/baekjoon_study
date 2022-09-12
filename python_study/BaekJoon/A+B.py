f609211554f1966dbcdf5b38321fff462f9a1feb
nbh0000 <68410718+nbh0000@users.noreply.github.com>
2022-09-12 14:20:15 +0900

    Delete python_study directory

--- python_study/BaekJoon/A+B.py
+++
@@ -1,9 +0,0 @@
-#a = input().split()
-#print(int(a[0])+int(a[1]))
-
-#a, b = input().split()
-#print(int(a)+int(b))
-a = int(input())
-if (20>a>10):
-    print(a)
-    
--- python_study/Study/len.py
+++
@@ -1,6 +0,0 @@
-n = 3000
-s =""
-for i in range(1,n+1):
-    s = s+str(i)
-
-print(len(s))
